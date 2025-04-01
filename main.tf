659382de8e4f1f2b0ffbe0f4f610b1bae3c5f870
rsclarke <hey@rsclarke.dev>
2025-04-01 11:39:24 +0800

    Update to cloudflare v5

--- main.tf
+++ main.tf
@@ -1,20 +1,23 @@
-resource "cloudflare_record" "spf" {
+resource "cloudflare_dns_record" "spf" {
   zone_id = var.zone_id
   name    = "@"
-  value   = "v=spf1 -all"
+  content = "v=spf1 -all"
   type    = "TXT"
+  ttl     = 1
 }
 
-resource "cloudflare_record" "dkim" {
+resource "cloudflare_dns_record" "dkim" {
   zone_id = var.zone_id
   name    = "*._domainkey"
-  value   = "v=DKIM1; p="
+  content = "v=DKIM1; p="
   type    = "TXT"
+  ttl     = 1
 }
 
-resource "cloudflare_record" "dmarc" {
+resource "cloudflare_dns_record" "dmarc" {
   zone_id = var.zone_id
   name    = "_dmarc"
-  value   = "v=DMARC1; p=reject; rua=mailto:${join(",mailto:", var.dmarc_rua)}; ruf=mailto:${join(",mailto:", var.dmarc_ruf)}; fo=1:d:s"
+  content = "v=DMARC1; p=reject; rua=mailto:${join(",mailto:", var.dmarc_rua)}; ruf=mailto:${join(",mailto:", var.dmarc_ruf)}; fo=1:d:s"
   type    = "TXT"
+  ttl     = 1
 }
